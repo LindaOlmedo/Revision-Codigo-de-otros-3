af0ed5b1d9ff64bffe53e59b58eb0bfc54992ce6
LindaOlmedo <lindarocioolmedoreyes@gmail.com>
2024-07-17 18:05:49 -0600

    codigo3

--- filtro.js
+++ filtro.js
@@ -1,21 +1,27 @@
 // Tenemos un li de productos
 
-const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+// esta mal escrito el objeto
+const productos = [ productos ];
+productos.push( {
+
+  // No poner llaves {} de inicio o cierre
+  nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg",
+  nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg",
+  nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg",
+  nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg",
+  nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg",
+}  ); // Se debe utilizar llaves {} , no []
 
 const li = document.getElementsByName("lista-de-productos")
 const $i = document.querySelector('.input');
 
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
+  // no usar variable var
+  const d = document.createElement("div")
   d.classList.add("producto")
 
-  var ti = document.createElement("p")
+  // no usar variable var
+  const ti = document.createElement("p")
   ti.classList.add("titulo")
   ti.textContent = productos[i].nombre
   
@@ -31,24 +37,29 @@ for (let i = 0; i < productos.length; i++) {
 displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
-botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
+botonDeFiltro.onclick = function()
+// while no  
+    (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = $i.value; 
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
 
+  // falta abrir la llave {}
+  const productosFiltrados = filtrado(productos, texto );
+  {
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
+    const d = document.createElement("div") // No usar variable (var) 
     d.classList.add("producto")
   
-    var ti = document.createElement("p")
+    // No usar variable (var)
+    const ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
     
-    var imagen = document.createElement("img");
+    // No usar variable (var)
+    const imagen = document.createElement("img"); 
     imagen.setAttribute('src', productosFiltrados[i].img);
   
     d.appendChild(ti)
